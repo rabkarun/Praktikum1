92dfd88d5ce7a68ff90f2c3a4bc069b77c1173c4
Akbar <cruz.bar05@gmail.com>
2018-11-02 19:44:57 +0700

    latihan ke 1

---
+++ latihan1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+
+
+ using namespace std;
+
+
+
+ int main() {
+
+	int p, l, luas;
+
+	cout << "masukan panjang: ";
+
+	cin >> p;
+
+	cout << "masukkan lebar: ";
+
+	cin >> l;
+
+
+
+	luas = p * l;
+
+
+
+	cout << "luas persegi panjang : " << luas << endl;
+
+	return 0;
+
+	}
